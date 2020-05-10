2d466909b451bfb980c72c097eae19dd61042e46
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-05-10 10:04:18 +0100

    set up native base

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading onFinish={() => setIsReady(true)} startAsync={loadFonts} />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +36,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
